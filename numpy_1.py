92bc6af0eda5d12d53ef354be19c439c67283385
Vinod Sebastian <vinod.mathew.sebastian@gmail.com>
2020-09-26 07:37:43 +0000

    File updated

--- numpy_1.py
+++ numpy_1.py
@@ -1,3 +1,7 @@
 import numpy as np
+# Converting a list to np array
 li = [[1,2,3],[4,5,6]]
 print(np.array(li))
+#generating a range of values with the optional distance parameter
+rnge = np.arange(1,28,3)
+print(rnge)
