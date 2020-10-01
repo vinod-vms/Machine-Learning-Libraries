0158e5dfacbdb6dc6c659947f107c0c8894269b7
Vinod Mathew Sebastian <vinod.mathew.sebastian@gmail.com>
2020-10-01 09:58:14 +0000

    pandas_1.py updated

--- pandas_1.py
+++ pandas_1.py
@@ -1,3 +1,11 @@
 import pandas as pd
 df = pd.read_csv('https://perso.telecom-paristech.fr/eagan/class/igr204/data/factbook.csv')
+
+#gist of the data
 print(df.head())
+
+#some statistical information
+print(df.describe ())
+
+#column headers
+print(df.columns.values)
