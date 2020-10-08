af9d465396e6ab86a25fda51a2effa774c43ea35
Vinod Mathew Sebastian <vinod.mathew.sebastian@gmail.com>
2020-10-08 06:33:48 +0000

    File matplotlib_1.py added

--- pandas_1.py
+++ pandas_1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('https://perso.telecom-paristech.fr/eagan/class/igr204/data/factbook.csv')
+df = pd.read_csv('https://perso.telecom-paristech.fr/eagan/class/igr204//factbook.csv')
 
 #gist of the data
 print(df.head())
@@ -9,3 +9,12 @@ print(df.describe ())
 
 #column headers
 print(df.columns.values)
+
+#columns to be selected
+#columns = ["Country", "Area (sq km)",sep="\t"]
+#columns = [*]
+
+#Selecting columns
+#df[columns]
+
+#df.loc[columns]
